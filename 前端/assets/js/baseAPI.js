1c9269639443775a8dc85645a25612dce911745b
zhangchu504 <505555203@qq.com>
2021-09-22 11:08:41 +0800

    Update baseAPI.js

--- 前端/assets/js/baseAPI.js
+++ 前端/assets/js/baseAPI.js
@@ -4,7 +4,7 @@
 $.ajaxPrefilter(function(options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
   // options.url = 'http://ajax.frontend.itheima.net' + options.url
-  options.url = 'http://1.117.201.132:3000' + options.url
+  options.url = 'http://127.0.0.1:3000' + options.url
 
   // 统一为有权限的接口，设置 headers 请求头
   if (options.url.indexOf('/my/') !== -1) {
